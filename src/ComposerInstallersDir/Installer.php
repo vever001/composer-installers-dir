d7d9c27468dda425a86bd603a38daf0a3df922b8
Herve Donner <hervedonner@gmail.com>
2019-08-02 21:44:56 +0200

    Use Filesystem::normalizePath().

--- src/ComposerInstallersDir/Installer.php
+++ src/ComposerInstallersDir/Installer.php
@@ -4,14 +4,18 @@ namespace vever001\ComposerInstallersDir;
 
 use Composer\Installers\Installer as ComposerInstaller;
 use Composer\Package\PackageInterface;
+use Composer\Util\Filesystem;
 
 class Installer extends ComposerInstaller {
 
   public function getInstallPath(PackageInterface $package) {
     $installPath = parent::getInstallPath($package);
+
     $extra = $this->composer->getPackage()->getExtra();
     if (!empty($extra['installer-dir'])) {
-      $installPath = rtrim($extra['installer-dir'], '/') . '/' . $installPath;
+      $filesystem = new Filesystem();
+      $installerDir = $filesystem->normalizePath($extra['installer-dir']);
+      $installPath = $installerDir . '/' . $installPath;
     }
 
     return $installPath;
